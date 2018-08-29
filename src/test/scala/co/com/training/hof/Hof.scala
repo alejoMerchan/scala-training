bd37651c95c2033dd37c5127e727543195f296fb
amerchan <amercha@velocitypartners.com>
2018-08-29 18:15:42 -0500

    First commit

--- src/test/scala/co/com/training/hof/Hof.scala
+++ src/test/scala/co/com/training/hof/Hof.scala
@@ -21,37 +21,46 @@ class Hof extends FunSuite {
 
   test("exercise 1") {
 
-    def helloWorld(f: () => Unit) = ???
+    val anyName : () => Unit = () => println("Hello World")
+    def helloWorld(f: () => Unit) = f
+    helloWorld(anyName)
 
   }
 
 
   test("exercise 2") {
 
-    def transformToString(f: Int => String, a: Int): String = ???
+    val test : Int => String = number => number.toString
+    def transformToString(f: Int => String, a: Int): String = f(a)
+    assert(transformToString(test, 6)=="6")
 
   }
+
 
   test("exercise 3") {
+    val sumFunction : (Int,Int) => Int = (num1, num2) => num1 + num2
+    def sumToNumbers(f: (Int, Int) => Int, a: Int, b: Int): Int = f(a,b)
 
-    def sumToNumbers(f: (Int, Int) => Int, a: Int, b: Int): Int = ???
-
+    assert(sumToNumbers(sumFunction, 1, 2) == 3)
   }
-
+/**
   test("exercise 4") {
 
     def printANumber(f: Int => (), a: Int) = ???
 
   }
+  */
 
 
   test("exercise 5") {
 
-    def sumToNumbersAndRestForOther(f: (Int, Int) => Int, a: Int, b: Int, c: Int): Int = ???
+    def sumToNumbersAndRestForOther(f: (Int, Int) => Int, a: Int, b: Int, c: Int): Int = f(a,b) - c
     // not use a val.
+    assert(sumToNumbersAndRestForOther((a,b)=>a+b,2,3,1)==4)
 
   }
 
+  /**
   test("exercise 6") {
 
     // composite functions
@@ -69,5 +78,5 @@ class Hof extends FunSuite {
 
   }
 
-
+*/
 }
